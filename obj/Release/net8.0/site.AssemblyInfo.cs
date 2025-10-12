6d29dfa947f8460aa39e12f0196ee1cb67681f11
Luciana Pereira <lucianapereira@AVAPC-499985246.localdomain>
2025-10-11 21:50:43 -0300

    Ajustando versão do sdk no arquivo deploy

--- obj/Release/net8.0/site.AssemblyInfo.cs
+++ obj/Release/net8.0/site.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("site")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf6135d6deeba480440e3c52ca20e6ee7ce47b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0fcc3ec592aa9d3d5b5b2afd9af3552d616fa72")]
 [assembly: System.Reflection.AssemblyProductAttribute("site")]
 [assembly: System.Reflection.AssemblyTitleAttribute("site")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
