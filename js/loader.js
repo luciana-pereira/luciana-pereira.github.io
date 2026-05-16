4f03c99601a2a3fce486c1ade45a29c9b84d68a3
luciana-pereira <luciana-pereira@users.noreply.github.com>
2026-05-16 18:17:21 +0000

    deploy: 14e2eca9878a6698ea5c73675151980d18f038f5

---
+++ js/loader.js
@@ -0,0 +1,55 @@
+window.loader = {
+
+    start: () => {
+
+        let progress = 0;
+
+        const progressBar =
+            document.querySelector(".loader-progress");
+
+        const percentage =
+            document.getElementById("loader-text");
+
+        const interval = setInterval(() => {
+
+            progress++;
+
+            if (progressBar)
+                progressBar.style.width =
+                    `${progress}%`;
+
+            if (percentage)
+                percentage.innerText =
+                    `${progress}%`;
+
+            if (progress >= 100)
+            {
+                clearInterval(interval);
+
+                setTimeout(() => {
+
+                    const loader =
+                        document.getElementById("initial-loader");
+
+                    if (loader)
+                    {
+                        loader.classList.add("hide");
+
+                        setTimeout(() => {
+                            loader.remove();
+                        }, 600);
+                    }
+
+                    document.body.classList.remove("loading");
+
+                }, 300);
+            }
+
+        }, 20);
+    }
+};
+
+window.addEventListener("load", () => {
+
+    window.loader.start();
+});
